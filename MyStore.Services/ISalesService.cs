b71e22c3741d492773463986b22944de602d864d
Samuel Branham <sbranham314@gmail.com>
2026-06-07 23:38:12 -0400

    Drop GetReceiptAsync from ISalesService

--- MyStore.Services/ISalesService.cs
+++ MyStore.Services/ISalesService.cs
@@ -10,6 +10,5 @@ public interface ISalesService
     Task<ApiResponse<List<Sale>>> GetSalesByDateRangeAsync(DateTime startDate, DateTime endDate, int companyId);
     Task<ApiResponse<Sale>> CreateSaleAsync(CreateSaleRequest request, int companyId);
     Task<ApiResponse<bool>> DeleteSaleAsync(int id, int companyId);
-    Task<ApiResponse<ReceiptResponse>> GetReceiptAsync(int id, int companyId);
 }
 
